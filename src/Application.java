6460c7722c460ceec15172fa68dfc98782c32a2c
Nahum-png <nahuolvera@gmail.com>
2020-09-09 20:01:19 -0500

    Mi asistencia

--- src/Application.java
+++ src/Application.java
@@ -1,26 +1,4 @@
 
-class Subject {
-    private String name;
-    private int credits;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getCredits() {
-        return credits;
-    }
-
-    public void setCredits(int credits) {
-        this.credits = credits;
-    }
-}
-
-
 class Professor {
     private String name;
 
